c07e84d4f4063fefbd2ad40f287e2c3630e18ac3
yy13004 <rlatnrud712@naver.com>
2026-02-24 03:07:41 +0000

    기사 초안 구조 완료

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 import streamlit as st
 
-st.title("내 웹페이지")
-st.write("여기에 기사를 입력할 수 있습니다.")
+st.set_page_config(layout="wide")
+
+st.title("‘빈틈없는’ 도시와 소상공인의 ‘빈자리’")
+
+st.subheader("""
+창고가 사라지고 사람이 모였다
+""")
+
+st.markdown("""
+
+""")
+
+
+st.components.v1.html("""
+<div style="width:100%; height:500px; background-color:#f0f0f0; display:flex; align-items:center; justify-content:center;">
+주말 유동인구 그래프
+</div>
+""", height=500)
